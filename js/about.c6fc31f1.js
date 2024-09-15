c6e1e9fe7cd46cbf538a3c5d7234153090b794b5
majdmohamad <majdmohamad115546@gmail.com>
2024-09-15 19:14:54 +0400

    Updates

---
+++ js/about.c6fc31f1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkelzero_web_school"]=self["webpackChunkelzero_web_school"]||[]).push([[594],{760:function(e,n,a){a.r(n),a.d(n,{default:function(){return i}});var t=a(641),u=a(33);const o={class:"about-ve"};function r(e,n,a,r,s,c){return(0,t.uX)(),(0,t.CE)("div",o,[(0,t.Lk)("h1",null,(0,u.v_)(e.pageName),1),(0,t.Lk)("p",null,(0,u.v_)(e.pageDescription),1)])}var s={data:function(){return{pageName:"About Us",pageDescription:"This Is About Us Page"}},name:"about-ve"},c=a(262);const l=(0,c.A)(s,[["render",r]]);var i=l}}]);
+//# sourceMappingURL=about.c6fc31f1.js.map
